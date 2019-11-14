eb48e128b023eabe72bbe1310cb5d8304930efae
nomadedge <nomad.edge@gmail.com>
2019-11-14 22:21:40 +0300

    Add migration for new PaymentId type

--- DTShop.OrderService.Data/Migrations/20191114191909_ChangePaymentIdType.Designer.cs
+++ DTShop.OrderService.Data/Migrations/20191114191909_ChangePaymentIdType.Designer.cs
@@ -4,14 +4,16 @@ using DTShop.OrderService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DTShop.OrderService.Data.Migrations
 {
     [DbContext(typeof(OrderDbContext))]
-    partial class OrderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191114191909_ChangePaymentIdType")]
+    partial class ChangePaymentIdType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DTShop.OrderService.Data/Migrations/20191114191909_ChangePaymentIdType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DTShop.OrderService.Data.Migrations
+{
+    public partial class ChangePaymentIdType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "PaymentId",
+                table: "Orders",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PaymentId",
+                table: "Orders",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldNullable: true);
+        }
+    }
+}
